27937776d95fdf86eece65fd01b78d0fca84de84
Egis Gajo <egisgajo28@gmail.com>
2023-08-02 16:05:17 +0200

    navbar title

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,10 @@
 function NavBar() {
-  return <div className="nav"></div>;
+  return (
+    <div className="nav">
+      <h1>Logo</h1>
+      <h2>Search</h2>
+    </div>
+  );
 }
 
 export default NavBar;
