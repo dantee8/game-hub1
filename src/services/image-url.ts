79253eafe13d72fa1f85a18e6652d6f3204e13c8
Egis Gajo <egisgajo28@gmail.com>
2023-08-06 18:08:16 +0200

    handle Games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,9 +1,11 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
 
+const getCroppedImageUrl = (url: string) => {
+  if (!url) return noImage;
 
-const getCroppedImageUrl = (url:string)=> {
-    const target = 'media/';
-    const index = url.indexOf(target) + target.length;
-    return   url.slice(0,index) + 'crop/600/400/' + url.slice(index)
-}
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
 
 export default getCroppedImageUrl;
